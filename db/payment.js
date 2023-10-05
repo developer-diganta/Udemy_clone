42b6ca08e66f8e2e54d0ec425c69fa4ea8d81c93
Diganta Kr Banik <digantabanik2000@gmail.com>
2023-10-05 17:55:52 +0530

    database: add schema for payment (under progress)

---
+++ db/payment.js
@@ -0,0 +1,26 @@
+/*
+*****SCHEMA FOR PAYMENT*****
+    
+    student: student who made payment, linked to student collection via student id
+
+    course: course which was bought, linked to course collection via course id
+
+    paymentDetails: to be filled later
+
+*/
+
+const Payment = {
+    student:{
+        type:mongoose.Schemas.Type.objectId,
+        ref:'Student'
+    },
+    course:{
+        type:mongoose.Schemas.Type.objectId,
+        ref:'Course'       
+    },
+    paymentDetails:{
+        // To be filled later depending on payment gateway implemented
+    }
+}
+
+export default Payment;
